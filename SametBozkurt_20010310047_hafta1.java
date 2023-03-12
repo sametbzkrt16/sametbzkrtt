dd9f84842157575460295b24d7619d96b1ba5c7e
Samet Bozkurt <127692941+sametbzkrt16@users.noreply.github.com>
2023-03-12 23:37:40 +0300

    Add files via upload
    hafta1 uygulama yuklendi

---
+++ SametBozkurt_20010310047_hafta1.java
@@ -0,0 +1,12 @@
+package _20010310047_Samet_Bozkurt;
+
+public class SametBozkurt_20010310047_hafta1 {
+
+	public static void main(String[] args) {
+	
+		System.out.print("merhb dünya);
+		// TODO Auto-generated method stub
+
+	}
+
+}
